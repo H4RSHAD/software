444d5b7b1eaff9d1f3ee2f886f6d57b7b525779b
Chinin0 <cristhianarauz06@gmail.com>
2024-05-10 12:22:44 -0400

    aumento de Dashboard en el archivo prueba.html

--- proyecto/database/usuario_db.py
+++ proyecto/database/usuario_db.py
@@ -17,12 +17,12 @@ def create(usuario: User) -> User:
 
 
 def login(usuario: User) -> User:
-    sql = " SELECT name, email, password_hash FROM users WHERE email = '{}' ".format(usuario.email) #la variable del modelo User
+    sql = " SELECT name, email, password_hash, create_at FROM users WHERE email = '{}' ".format(usuario.email) #la variable del modelo User
     row = _fetch_one(sql,None)
     print('---------- DAATOS DE CAPA DATO LOGIN')
     print(row)
     if row !=None:
-        usuario = User(row[0],row[1], (User.check_password(row[2],(usuario.password_hash))))
+        usuario = User(row[0],row[1], (User.check_password(row[2],(usuario.password_hash))),row[3])
         print(usuario)
         return usuario  # El usuario se encuentra en la BD_Lab
     else:
