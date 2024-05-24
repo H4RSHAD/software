28211ef41694e182d02430be9ad1329151b726a1
Franz <franzribera12@gmail.com>
2024-05-24 18:45:22 -0400

    update perfil

--- proyecto/controllers/UserController.py
+++ proyecto/controllers/UserController.py
@@ -6,5 +6,8 @@ def create(usuario: User) -> User:
     # falta implementar los metodos de validacion asi que hay que ingresar datos correctos sino genera error
     return usuario_db.create(usuario)
 
+def update(usuario: User) -> User:
+    return usuario_db.login(usuario)
+
 def login(usuario: User) -> User:
     return usuario_db.login(usuario)
