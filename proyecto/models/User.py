493a23e3752c604a0228f89eb5df66c3ed238b35
mendoza <Mendoza210727>
2024-06-21 16:26:13 -0400

    roles y planes

--- proyecto/models/User.py
+++ proyecto/models/User.py
@@ -6,6 +6,18 @@ from proyecto import app
 db = SQLAlchemy(app)        # creamos una instancia 
 
 
+
+#Define la tabla Roles
+class Roles(db.Model):
+    __tablename__ = 'roles'  # damos nombre a nuestra tabla
+    id = db.Column(db.Integer, primary_key=True)
+    rol = db.Column(db.String(255), nullable=False)
+    create_at = db.Column(db.DateTime, default=datetime.now)
+    update_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    
+    # Relación inversa con la tabla User
+    users = db.relationship('User', backref='role', lazy=True)
+
 # Define el modelo de datos Usuario
 class User(db.Model):
     __tablename__ = 'users'  # damos nombre a nuestra tabla
@@ -13,15 +25,19 @@ class User(db.Model):
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
+    id_rol = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)  # Llave foránea hacia la tabla roles
+    state = db.Column(db.String(255), nullable=False)
     create_at = db.Column(db.DateTime, default=datetime.now)
     update_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
     
     subscriptions = db.relationship('Subscription', backref='user', lazy=True)
 
-    def __init__(self, name, email, password,create_at):
+    def __init__(self, name, email, password, id_rol, state, create_at):
         self.name = name
         self.email = email
         self.password_hash = generate_password_hash(str(password))
+        self.id_rol = id_rol
+        self.state = state
         self.create_at = create_at
 
 
